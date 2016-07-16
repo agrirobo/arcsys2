a32f03c08acd8d321e5deb6502500bd1c33b93e8
YamaCIR-KIT <httperror@404-notfound.jp>
2016-07-16 17:37:32 +0900

    Update simple_trajectory_planner package

--- src/manipulator/simple_trajectory_planner/src/trajectory_planner.cpp
+++ src/manipulator/simple_trajectory_planner/src/trajectory_planner.cpp
@@ -7,42 +7,40 @@
 
 #include "servo_msgs/KrsServoDegree.h"
 
-namespace arc {
-  class TargetPlanner {
-  public:
-    static const SERVO_NUM = 3;
+class TargetPlanner {
+public:
+  static const unsigned int SERVO_ID_MAX = 3;
 
-    TargetPlanner()
-      : transform_buffer(),
-        transform_listener(transform_buffer)
-    {
-      for (int i=0; i<4; i++) target[i].id = i;
+  TargetPlanner()
+    : transform_buffer(),
+    transform_listener(transform_buffer)
+  {
+    for (int i=0; i<SERVO_ID_MAX; i++) target[i].id = i;
+  }
+
+  ~TargetPlanner() {}
+
+  void plan(void) {
+    if (getTransform("/tomato", "arm_base")) calc();
+  }
+
+private:
+  tf2_ros::Buffer transform_buffer;
+  tf2_ros::TransformListener transform_listener;
+
+  geometry_msgs::TransformStamped transform_stamped;
+  std::vector<servo_msgs::KrsServoDegree> target[4];
+
+  bool getTransform(const std::string& target, const std::string& source) {
+    try {
+      transform_stamped = transform_buffer.lookupTransform(target, source, ros::Time(0));
+    } catch (tf2::TransformException& exception) {
+      ROS_WARN("%s", exception.what());
+      return false;
     }
-
-    ~TargetPlanner() {}
-
-    void plan(void) {
-      if (getTransform("/tomato", "arm_base")) calc();
-    }
-
-  private:
-    tf2_ros::Buffer transform_buffer;
-    tf2_ros::TransformListener transform_listener;
-
-    geometry_msgs::TransformStamped transform_stamped;
-    std::vector<servo_msgs::KrsServoDegree> target[4];
-
-    bool getTransform(const std::string& target, const std::string& source) {
-      try {
-        transform_stamped = transform_buffer.lookupTransform("target", "source", ros::Time(0));
-      } catch (tf2::TransformException& exception) {
-        ROS_WARN("%s", exception.what());
-        return false;
-      }
-      return true;
-    }
-  };
-}
+    return true;
+  }
+};
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "trajectory_planner");
@@ -51,13 +49,12 @@ int main(int argc, char** argv) {
 
   ros::Rate rate(10.0);
 
-  arc::TargetPlanner planner;
+  TargetPlanner planner;
 
   while (handle,ok()) {
     planner.plan();
     rate.sleep();
   }
-
 
   return 0;
 }
