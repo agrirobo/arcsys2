5d33d0c04de8e703be1c7031c97f77afb2915033
YamaCIR-KIT <httperror@404-notfound.jp>
2016-07-16 19:38:26 +0900

    Update simple_trajectory_planner package

--- src/manipulator/simple_trajectory_planner/src/trajectory_planner.cpp
+++ src/manipulator/simple_trajectory_planner/src/trajectory_planner.cpp
@@ -10,49 +10,74 @@
 class TargetPlanner {
 public:
   static const unsigned int SERVO_ID_MAX = 3;
+  static const double FRAME_LENGTH = 1.0;
 
   TargetPlanner()
     : transform_buffer(),
-    transform_listener(transform_buffer)
+      transform_listener(transform_buffer)
   {
-    for (int i=0; i<SERVO_ID_MAX; i++) target[i].id = i;
+    for (int i=0; i<SERVO_ID_MAX; i++) servo[i].id = i;
+    frame_length = FRAME_LENGTH;
   }
 
-  ~TargetPlanner() {}
+  explicit TargetPlanner(double desired_length)
+    : transform_buffer(),
+      transform_listener(transform_buffer)
+  {
+    for (int i=0; i<SERVO_ID_MAX; i++) servo[i].id = i;
+    frame_length = desired_length;
+  }
 
-  void plan(void) {
-    if (getTransform("/tomato", "arm_base")) calc();
+  ~TargetPlanner()
+  {}
+
+  bool plan(void) {
+    try {
+      transform_stamped
+        = transform_buffer.lookupTransform("/tomato", "/arm_base", ros::Time(0));
+    } catch (tf2::TransformException& exception) {
+      ROS_ERROR("%s", exception.what());
+      return false;
+    }
+
+    std_msgs::float64 sqrt
+      = std::sqrt(std::pow(transform_stamped.transform.translation.x)
+        + std::sqrt(std::pow(transform_stamped.transform.translation.y);
+
+    servo[0].angle
+      = std::atan(transform_stamped.transfrom.translation.y
+                  / transform_stamped.transfrom.translation.x)
+        + std::acos(sqrt / (2 * frame_length));
+
+    servo[1].angle = 2 * servo[0].angle;
+    servo[2].angle = -servo[0].angle;
+
+    return true;
   }
 
 private:
-  tf2_ros::Buffer transform_buffer;
-  tf2_ros::TransformListener transform_listener;
+  tf2::Buffer transform_buffer;
+  tf2::TransformListener transform_listener;
 
   geometry_msgs::TransformStamped transform_stamped;
-  std::vector<servo_msgs::KrsServoDegree> target[4];
-
-  bool getTransform(const std::string& target, const std::string& source) {
-    try {
-      transform_stamped = transform_buffer.lookupTransform(target, source, ros::Time(0));
-    } catch (tf2::TransformException& exception) {
-      ROS_WARN("%s", exception.what());
-      return false;
-    }
-    return true;
-  }
+  std::vector<servo_msgs::KrsServoDegree> servo[SERVO_ID_MAX];
+  double frame_length;
 };
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "trajectory_planner");
-  ros::NodeHandle handle;
-  ros::Publisher pub = handle.advertise<servo_msgs::KrsServoDegree>("krs_servo", 16);
+  ros::NodeHandle node_handle;
+  ros::Publisher publisher = node_handle.advertise<servo_msgs::KrsServoDegree>("krs_servo", 16);
 
   ros::Rate rate(10.0);
 
   TargetPlanner planner;
 
-  while (handle,ok()) {
-    planner.plan();
+  while (node_handle.ok()) {
+    if (planner.plan())
+      for (int id = 0; id < planner.SERVO_ID_MAX; id++)
+        publisher.publish(planner.servo[id]);
+
     rate.sleep();
   }
 
