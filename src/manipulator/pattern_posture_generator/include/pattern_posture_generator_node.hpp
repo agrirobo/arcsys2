eff81cfb29ba91588ceb8084342aeb0a24306de7
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-09-17 04:28:27 +0900

    Add figure of PatternKeyPosture service on hpp.

--- src/manipulator/pattern_posture_generator/include/pattern_posture_generator_node.hpp
+++ src/manipulator/pattern_posture_generator/include/pattern_posture_generator_node.hpp
@@ -1,5 +1,24 @@
 #ifndef __PATTERN_POSTURE_GENERATOR_NODE_H_INCLUDE__
 #define __PATTERN_POSTURE_GENERATOR_NODE_H_INCLUDE__
 
+#include <vector>
+#include <map>
+
+#include "pattern_posture_generator/PatternKeyPosture.h"
+
+class PatternPostureGenerator {
+private:
+  PatternPostureGenerator();
+public:
+  explicit PatternPostureGenerator(ros::NodeHandle& nh);
+
+private:
+  bool getPostureKey(pattern_posture_generator::PatternKeyPosture::Request&  req,
+                     pattern_posture_generator::PatternKeyPosture::Response& res);
+
+private:
+  ros::ServiceServer key_srv;
+  std::map<std::string, std::vector<double> > posture_datas;
+};
 
 #endif
