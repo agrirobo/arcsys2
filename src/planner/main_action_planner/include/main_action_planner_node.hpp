106a89ca9cd186b5788d061456ae0eb6bdf39041
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-09-18 04:27:59 +0900

    Add include guard on MAP.

--- src/planner/main_action_planner/include/main_action_planner_node.hpp
+++ src/planner/main_action_planner/include/main_action_planner_node.hpp
@@ -1,3 +1,8 @@
+#ifndef __MAIN_ACTION_PLANNER_NODE_H_INCLUDE__
+#define __MAIN_ACTION_PLANNER_NODE_H_INCLUDE__
+
 #include "ros/ros.h"
 
 #include "pattern_posture_generator/PatternPosture.h"
+
+#endif
