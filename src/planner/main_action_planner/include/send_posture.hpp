c3f6214b2139a823d9ada7fce2947e1f3c0e2003
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-09-18 05:50:14 +0900

    Add factory func of SendPosture on MAP

--- src/planner/main_action_planner/include/send_posture.hpp
+++ src/planner/main_action_planner/include/send_posture.hpp
@@ -2,12 +2,15 @@
 #define __SEND_POSTURE_H_INCLUDE__
 
 #include <vector>
+#include <string>
 
 class SendPosture {
 public:
   virtual ~SendPosture(){};
-  virtual void sendPosture(std::vector<double>& posture);
+  virtual void sendPosture(std::vector<double>& posture) = 0;
 }
+
+SendPosture* getSendPosture(std::string);
 
 #endif
 
