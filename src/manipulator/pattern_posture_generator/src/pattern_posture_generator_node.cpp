2ea444e8403f78fbd22cb9066bf46c152878b538
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-09-17 22:21:29 +0900

    Fix presence check param & Add ehco more info

--- src/manipulator/pattern_posture_generator/src/pattern_posture_generator_node.cpp
+++ src/manipulator/pattern_posture_generator/src/pattern_posture_generator_node.cpp
@@ -2,6 +2,17 @@
 
 #include "pattern_posture_generator_node.hpp"
 
+void dumpMap(std::map<std::string, std::string>& map) {
+  std::string map_info;
+  for (std::map<std::string, std::string>::iterator it = map.begin(), end_it = map.end();
+       it != end_it;
+       it++) {
+    map_info.append(it->second);
+    map_info.push_back(':');
+  }
+  ROS_INFO("%s", map_info.c_str());
+}
+
 int main(int argc, char* argv[]) {
   ros::init(argc, argv, "pattern_posture_generator");
   ros::NodeHandle nh;
@@ -16,15 +27,18 @@ int main(int argc, char* argv[]) {
 PatternPostureGenerator::PatternPostureGenerator(){}
 
 PatternPostureGenerator::PatternPostureGenerator(ros::NodeHandle& nh) {
-  if (!nh.getParam("pattern", pattern_names)) return;
+  if (!nh.hasParam("pattern")) throw;
+  nh.getParam("pattern", pattern_names);
   ROS_INFO("Get map of patterns parent");
-  for (std::map<std::string, std::string >::iterator it = pattern_names.begin();
-       it != pattern_names.end();
+  dumpMap(pattern_names);
+  for (std::map<std::string, std::string>::iterator it = pattern_names.begin(), end_it = pattern_names.end();
+       it != end_it;
        it++) {
     std::vector<double> posture;
-    if (!nh.getParam(std::string("pattern/").append(it->second), posture)) return;
+    std::string posture_param_name = std::string("pattern/").append(it->second);
+    nh.getParam(posture_param_name, posture);
     std::copy(posture.begin(), posture.end(), std::back_inserter(posture_datas[it->second]));
-    ROS_INFO(std::string("Found posture of ").append(it->second).c_str());
+    ROS_INFO("Found posture of [%s]", it->second.c_str());
   }
   key_srv = nh.advertiseService("getPostureKey", &PatternPostureGenerator::getPostureKey, this);
 }
