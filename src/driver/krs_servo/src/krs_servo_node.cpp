7455b643cafbe3051927b4537af8bbd3e2d98669
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-09-18 03:46:40 +0900

    Add last '\n' on file end.

--- src/driver/krs_servo/src/krs_servo_node.cpp
+++ src/driver/krs_servo/src/krs_servo_node.cpp
@@ -43,3 +43,4 @@ KrsServoNode::KrsServoNode(ros::NodeHandle& nh, const char* path)
 void KrsServoNode::krsServoDegreeCallback(const servo_msgs::KrsServoDegree::ConstPtr& msg) {
   krs.sendAngle(msg->id, msg->angle);
 }
+
