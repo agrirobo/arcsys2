3d4f1a9bdc36c97dd76823107832f49a4b59365f
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-09-18 05:46:53 +0900

    Add factory func of RequestPosture on MAP.

--- src/planner/main_action_planner/include/request_posture.hpp
+++ src/planner/main_action_planner/include/request_posture.hpp
@@ -2,12 +2,15 @@
 #define __REQUEST_POSTURE_H_INCLUDE__
 
 #include <vector>
+#include <string>
 
 class RequestPosture {
 public:
   virtual ~RequestPosure(){};
   virtual void requestPosture(std::vector<double>& posture) = 0;
 }
+
+RequestPosture* getRequestPosture(std::string name);
 
 #endif
 
