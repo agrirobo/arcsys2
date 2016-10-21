4ea275e3a6ee7b2a522241d4340058317650f623
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-21 22:07:07 +0900

    Add error message

--- src/planner/main_action_planner/src/main_action_planner_node.cpp
+++ src/planner/main_action_planner/src/main_action_planner_node.cpp
@@ -9,13 +9,13 @@ int main(int argc, char** argv) {
     auto posture_key = status.getPostureKey();
     RequestPosture* rp_p = RequestPostureFactory::get(posture_key[0], nh);
     if (!rp_p) {
-      ROS_INFO("Error: get nullptr by RP Factory");
+      ROS_INFO("Error: get nullptr by RP Factory: arg is [%s]", posture_key[0].c_str());
       return -1;
     }
 
     SendPosture* sp_p = SendPostureFactory::get(posture_key[1], nh);
     if (!sp_p) {
-      ROS_INFO("Error: get nullptr by SP Factory");
+      ROS_INFO("Error: get nullptr by SP Factory: arg is [%s]", posture_key[1].c_str());
       return -1;
     }
 
