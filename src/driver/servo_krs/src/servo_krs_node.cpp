7296adeed26e0164354acfa3d85eebbb994e2492
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-10-23 12:50:13 +0900

    Add error message

--- src/driver/servo_krs/src/servo_krs_node.cpp
+++ src/driver/servo_krs/src/servo_krs_node.cpp
@@ -2,7 +2,7 @@
 #include<ics3/ics>
 #include<servo_msgs/IdBased.h>
 
-ics::ICS3* driver;
+ics::ICS3* driver {nullptr};
 
 void move(const servo_msgs::IdBased::ConstPtr& msg) {
   auto degree = ics::Angle::newDegree(msg->angle);
@@ -17,8 +17,14 @@ int main(int argc, char** argv) {
   ros::NodeHandle pn {"~"};
   std::string path {"ttyUSB0"};
   pn.param<std::string>("path", path, path);
-  ics::ICS3 ics {path.c_str()};
-  driver = &ics;
+  try {
+    ics::ICS3 ics {path.c_str()};
+    driver = &ics;
+  } catch (std::runtime_error e) {
+    ROS_INFO("Error: Cannot make ICS3 instance [%s]", e.what());
+    ROS_INFO("I tried open [%s]", path.c_str());
+    return -1;
+  }
   ros::Subscriber sub = n.subscribe("krs", 100, move);
   ros::spin();
   return 0;
