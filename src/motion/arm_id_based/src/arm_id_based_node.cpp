af7840cf3d83cc1aa7aafb79bc80c8731e95c925
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-11-10 00:28:39 +0900

    Impliment callback function of arm_roll topic

--- src/motion/arm_id_based/src/arm_id_based_node.cpp
+++ src/motion/arm_id_based/src/arm_id_based_node.cpp
@@ -7,16 +7,18 @@
 void armMsgCb(const arm_msgs::ArmAnglesDegree::ConstPtr& msg);
 
 ros::Publisher pub;
+std::vector<int> id_vec;
+std::vector<int>::size_type length; // cache of id_vec length
 
 int main(int argc, char* argv[]) {
   ros::init(argc, argv, "arm_id_based_node");
   ros::NodeHandle pnh("~");
-  std::vector<int> id_vec;
   pnh.getParam("id", id_vec);
   if (id_vec.empty()) {
     ROS_ERROR("I need id parameter: id is int vector");
     return 1;
   }
+  length = id_vec.size();
   ros::NodeHandle nh;
   ros::Subscriber sub = nh.subscribe("arm_roll", 5, armMsgCb);
   pub = nh.advertise<servo_msgs::IdBased>("cmd_krs", 10);
@@ -26,5 +28,14 @@ int main(int argc, char* argv[]) {
 }
 
 void armMsgCb(const arm_msgs::ArmAnglesDegree::ConstPtr& msg) {
-  
+  if (msg->angles.size() != length) {
+    ROS_ERROR("Receive length not equal id vector's: receive [%lu] / id_vec [%lu]", msg->angles.size(), id_vec.size());
+    return;
+  }
+  for (std::size_t i = 0; i < length; i++) {
+    servo_msgs::IdBased send;
+    send.id = id_vec[i];
+    send.angle = msg->angles[i];
+    pub.publish(send);
+  }
 }
