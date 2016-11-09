2662d8f7e18c3699fbf7dc1a7a94096f5a80b8d0
YamaCIR-KIT <httperror@404-notfound.jp>
2016-11-09 18:27:51 +0900

    Update test.sh

--- src/motion/manipulator/test.sh
+++ src/motion/manipulator/test.sh
@@ -3,9 +3,13 @@
 if [ -z $1 ]; then src="arcsys2"; else src=$(echo $1 | sed -e "s/\.[^.]*$//g"); fi
 pkgdir=$(cd "$(dirname $0)"; pwd)
 
-rosrun xacro xacro.py ${pkgdir}/urdf/${src}.xacro > ${pkgdir}/urdf/${src}.urdf && check_urdf ${pkgdir}/urdf/${src}.urdf
+rosrun xacro xacro.py ${pkgdir}/urdf/${src}.xacro > ${pkgdir}/urdf/${src}.urdf || exit 1
 
-echo "launch? [y/n]" && read yn
+if [ check_urdf ${pkgdir}/urdf/${src}.urdf ]; then
+  echo "launch? [y/n]" && read yn;
+else
+  exit 1;
+fi
 
 case $yn in
   "y") roslaunch manipulator manipulator.launch ;;
