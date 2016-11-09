9e6627cfa5809c15828d0525bc685d6feb55ecde
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-11-09 22:25:16 +0900

    Apply joint name of motion/manipurator

--- src/state/arcsys2_controller/src/arcsys2_controller_node.cpp
+++ src/state/arcsys2_controller/src/arcsys2_controller_node.cpp
@@ -1,6 +1,23 @@
 #include"ros/ros.h"
-#include"arcsys2_controller/arcsys2_hw.hpp"
-#include"controller_manager/controller_manager.h"
+#include"hardware_interface/joint_command_interface.h"
+#include"hardware_interface/joint_state_interface.h"
+#include"hardware_interface/robot_hw.h"
+
+class Arcsys2HW : public hardware_interface::RobotHW {
+public:
+  Arcsys2HW();
+  void read();
+  void write();
+  ros::Time getTime() const;
+  ros::Duration getPeriod() const;
+private:
+  hardware_interface::JointStateInterface jntStateInterface;
+  hardware_interface::PositionJointInterface jntPosInterface;
+  double cmd[2];
+  double pos[2];
+  double vel[2];
+  double eff[2];
+};
 
 int main(int argc, char *argv[]) {
   ros::init(argc, argv, "arcsys2_controller_node");
@@ -24,3 +41,39 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
+inline Arcsys2HW::Arcsys2HW() {
+  // [input] connect and register the joint state interface
+  hardware_interface::JointStateHandle stateHandle0to1("arm0->arm1", &pos[0], &vel[0], &eff[0]);
+  jntStateInterface.registerHandle(stateHandle0to1);
+
+  hardware_interface::JointStateHandle stateHandle1to2("arm1->arm2", &pos[1], &vel[1], &eff[1]);
+  jntStateInterface.registerHandle(stateHandle1to2);
+
+  registerInterface(&jntStateInterface);
+
+  // [output] connect and register the joint position interface
+  hardware_interface::JointHandle posHandle0to1(jntStateInterface.getHandle("arm0->arm1"), &cmd[0]);
+  jntPosInterface.registerHandle(posHandle0to1);
+
+  hardware_interface::JointHandle posHandle1to2(jntStateInterface.getHandle("arm1->arm2"), &cmd[1]);
+  jntPosInterface.registerHandle(posHandle1to2);
+
+  registerInterface(&jntPosInterface);
+}
+
+inline void Arcsys2HW::read() {
+  // TODO: read encoder
+}
+
+inline void Arcsys2HW::write() {
+  // TODO: write KRS servo and base motor
+  // TODO: receive KRS angles
+}
+
+inline ros::Time Arcsys2HW::getTime() const {
+  return ros::Time::now();
+}
+
+inline ros::Duration Arcsys2HW::getPeriod() const {
+  return ros::Duration(0.01);
+}
