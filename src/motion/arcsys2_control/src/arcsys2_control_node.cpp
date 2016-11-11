83e8caea2d55c7eec467cfc3563b08a4bb343ff4
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-11-11 21:18:36 +0900

    Rename menber name to krs from arm

--- src/motion/arcsys2_control/src/arcsys2_control_node.cpp
+++ src/motion/arcsys2_control/src/arcsys2_control_node.cpp
@@ -7,6 +7,7 @@
 #include"ics3/ics"
 
 #include<string>
+#include<utility>
 
 class Arcsys2HW : public hardware_interface::RobotHW {
 public:
@@ -21,10 +22,10 @@ private:
   // for RobotHW
   hardware_interface::JointStateInterface jntStateInterface;
   hardware_interface::PositionJointInterface jntPosInterface;
-  double arm_cmd[2];
-  double arm_pos[2];
-  double arm_vel[2];
-  double arm_eff[2];
+  double krs_cmd[2];
+  double krs_pos[2];
+  double krs_vel[2];
+  double krs_eff[2];
 };
 
 int main(int argc, char *argv[]) {
@@ -33,7 +34,7 @@ int main(int argc, char *argv[]) {
   ros::NodeHandle pnh {"~"};
   std::string krs_path {"/dev/ttyUSB0"};
   pnh.param<std::string>("krs_path", krs_path, krs_path);
-  Arcsys2HW robot {krs_path};
+  Arcsys2HW robot {std::move(krs_path)};
   controller_manager::ControllerManager cm {&robot};
 
   ros::Rate rate(1.0 / robot.getPeriod().toSec());
@@ -57,19 +58,19 @@ inline Arcsys2HW::Arcsys2HW(const std::string& krs_path)
   jntPosInterface {}
 {
   // [input] connect and register the joint state interface
-  hardware_interface::JointStateHandle stateHandle0to1 {"arm0->arm1", &arm_pos[0], &arm_vel[0], &arm_eff[0]};
+  hardware_interface::JointStateHandle stateHandle0to1 {"arm0->arm1", &krs_pos[0], &krs_vel[0], &krs_eff[0]};
   jntStateInterface.registerHandle(stateHandle0to1);
 
-  hardware_interface::JointStateHandle stateHandle1to2 {"arm1->arm2", &arm_pos[1], &arm_vel[1], &arm_eff[1]};
+  hardware_interface::JointStateHandle stateHandle1to2 {"arm1->arm2", &krs_pos[1], &krs_vel[1], &krs_eff[1]};
   jntStateInterface.registerHandle(stateHandle1to2);
 
   registerInterface(&jntStateInterface);
 
   // [output] connect and register the joint position interface
-  hardware_interface::JointHandle posHandle0to1 {jntStateInterface.getHandle("arm0->arm1"), &arm_cmd[0]};
+  hardware_interface::JointHandle posHandle0to1 {jntStateInterface.getHandle("arm0->arm1"), &krs_cmd[0]};
   jntPosInterface.registerHandle(posHandle0to1);
 
-  hardware_interface::JointHandle posHandle1to2 {jntStateInterface.getHandle("arm1->arm2"), &arm_cmd[1]};
+  hardware_interface::JointHandle posHandle1to2 {jntStateInterface.getHandle("arm1->arm2"), &krs_cmd[1]};
   jntPosInterface.registerHandle(posHandle1to2);
 
   registerInterface(&jntPosInterface);
