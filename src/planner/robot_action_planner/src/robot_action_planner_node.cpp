561122e764794e4ad68b38b7ba8987492c6a1101
YamaCIR-KIT <httperror@404-notfound.jp>
2016-11-17 12:28:03 +0900

    Change almost all of code
    なんか動かねえなと思って色々いじくってる内に原型が無くなった
    変更前のコードは一気に書こうとしたのが悪かった

--- src/planner/robot_action_planner/src/robot_action_planner_node.cpp
+++ src/planner/robot_action_planner/src/robot_action_planner_node.cpp
@@ -1,3 +1,5 @@
+#include <string>
+
 #include <ros/ros.h>
 #include <moveit/move_group_interface/move_group.h>
 
@@ -6,42 +8,42 @@
 
 class MoveGroupPlanner {
   ros::NodeHandle nh;
+  ros::Publisher  pub;
+  ros::Subscriber sub;
 
-  moveit::planning_interface::MoveGroup              move_group;
-  moveit::planning_interface::MoveGroup::Plan        move_group_plan;
-  // moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
-
-  moveit_msgs::DisplayTrajectory display_trajectory;
-
-  ros::Publisher  display_publisher;
-  ros::Subscriber subscriber;
+  moveit::planning_interface::MoveGroup group;
+//   moveit::planning_interface::MoveGroup::Plan plan;
 
 public:
-  MoveGroupPlanner(const std::string& group)
-    : move_group { group },
-      display_publisher  { nh.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true) },
-      subscriber { nh.subscribe<geometry_msgs::Pose>("topic", 10, callback) }
+  MoveGroupPlanner(const std::string& name)
+    : pub   { nh.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true) },
+      sub   { nh.subscribe<geometry_msgs::Pose>("/arcsys2/tomapo", 1, &MoveGroupPlanner::callback, this) },
+      group { name }
   {
-    ROS_INFO("Reference frame: %s", move_group.getPlanningFrame().c_str());
-    ROS_INFO("Reference frame: %s", move_group.getEndEffectorFrame().c_str());
+    ROS_INFO_STREAM("Reference frame: " << group.getPlanningFrame());
+    ROS_INFO_STREAM("Reference frame: " << group.getEndEffectorLink());
   }
 
-private:
   void callback(const geometry_msgs::Pose::ConstPtr& msg) {
-    move_group.setPoseTarget(msg);
+    ROS_INFO_STREAM("Received message "                       << std::endl <<
+                    "geometry_msgs::Pose msg"                 << std::endl <<
+                    "  msg.position.x: " << (*msg).position.x << std::endl <<
+                    "  msg.position.y: " << (*msg).position.y << std::endl <<
+                    "  msg.position.z: " << (*msg).position.z << std::endl);
+//     group.setPoseTarget(target_pose);
 
-    if (bool success = move_group(move_group_plan)) {
-      ROS_INFO("Visualizing plan (pose goal) %s", success ? "" : "FAILED");
-      sleep(5.0);
-    }
-
-    ROS_INFO("Visualizing plan (again)");
-    display_trajectory.trajectory_start = move_group_plan.start_state_;
-    display_trajectory.trajectory.push_back(move_group_plan.trajectory_);
-    display_publisher.publish(display_trajectory);
-    sleep(5.0);
-
-    move_group.move();
+//     if (bool success = group.plan(plan)) {
+//       ROS_INFO("Visualizing plan (pose goal) %s", success ? "" : "FAILED");
+//       sleep(5.0);
+//     }
+// 
+//     ROS_INFO("Visualizing plan (again)");
+//     display_trajectory.trajectory_start = plan.start_state_;
+//     display_trajectory.trajectory.push_back(plan.trajectory_);
+//     pub.publish(display_trajectory);
+//     sleep(5.0);
+// 
+//     group.move();
   }
 };
 
@@ -50,7 +52,10 @@ int main(int argc, char** argv) {
 
   MoveGroupPlanner planner {"arcsys2"};
 
-  ros::spin();
+  while (ros::ok()) {
+    ros::spin();
+    sleep(5.0);
+  }
 
   return 0;
 }
