47f95fc9787685ca9c4c6619e8ec5987396c4c08
YamaCIR-KIT <httperror@404-notfound.jp>
2016-11-19 13:25:11 +0900

    Still exist the error

--- src/planner/arcsys2_move_group/src/arcsys2_move_group.cpp
+++ src/planner/arcsys2_move_group/src/arcsys2_move_group.cpp
@@ -1,56 +1,62 @@
 #include <string>
+#include <vector>
 
 #include <ros/ros.h>
 #include <moveit/move_group_interface/move_group.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 
 #include <geometry_msgs/Pose.h>
+#include <moveit_msgs/DisplayRobotState.h>
 #include <moveit_msgs/DisplayTrajectory.h>
 
 class MoveGroupPlanner {
-  ros::NodeHandle nh;
-  ros::Publisher  pub;
-  ros::Subscriber sub;
+  ros::NodeHandle   node;
+  ros::Publisher    publisher;
+  ros::Subscriber   subscriber;
 
-  moveit::planning_interface::MoveGroup              group;
+  const std::string group_name;
+
+  moveit::planning_interface::MoveGroup              move_group;
   moveit::planning_interface::MoveGroup::Plan        plan;
   moveit::planning_interface::PlanningSceneInterface scene;
-
-  geometry_msgs::Pose initial_pose;
 
   moveit_msgs::DisplayTrajectory dpy;
 
 public:
   MoveGroupPlanner(const std::string& name)
-    : pub   { nh.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true) },
-      sub   { nh.subscribe<geometry_msgs::Pose>("sub_topic", 1, &MoveGroupPlanner::callback, this) },
-      group { name },
-      initial_pose { group.getCurrentPose().pose }
+    : publisher  { node.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path", 1, true) },
+      subscriber { node.subscribe<geometry_msgs::Pose>("sub_topic", 1, &MoveGroupPlanner::callback, this) },
+      group_name { name },
+      move_group { group_name }
   {
-    // ROS_INFO_STREAM("reference frame: " << group.getPlanningFrame());
-    // ROS_INFO_STREAM("reference frame: " << group.getEndEffectorLink());
-    ROS_INFO_STREAM("initial pose: " << initial_pose);
-
-    group.setPlannerId("RRTConnectkConfigDefault");
+    move_group.setPlannerId("RRTConnectkConfigDefault");
   }
 
   void callback(const geometry_msgs::Pose::ConstPtr& target_pose) {
-    // ROS_INFO_STREAM("message received but don't use this for test");
+    move_group.setStartStateToCurrentState();
 
-    // geometry_msgs::Pose random_pose = group.getRandomPose().pose;
-    // ROS_INFO_STREAM("random_pose:"                                          << std::endl <<
-    //                 "  random_pose.position.x: " << random_pose.position.x  << std::endl <<
-    //                 "  random_pose.position.y: " << random_pose.position.y  << std::endl <<
-    //                 "  random_pose.position.z: " << random_pose.position.z  << std::endl);
+    ROS_INFO_STREAM("target pose received"             << std::endl
+                    << getPartitionString('=', 40)     << std::endl
+                    << move_group.getPoseTarget().pose
+                    << getPartitionString('=', 40)     << std::endl);
 
-    // ROS_INFO_STREAM(group.getCurrentPose());
-    // ROS_INFO_STREAM(group.getGoalPositionTolerance());
+    move_group.setPoseTarget(*target_pose, move_group.getEndEffectorLink());
 
-    group.setPoseTarget(*target_pose, group.getEndEffectorLink());
+    if (move_group.plan(plan)) {
+      dpy.trajectory_start = plan.start_state_;
+      dpy.trajectory.push_back(plan.trajectory_);
+      publisher.publish(dpy);
+    }
 
-    if (!group.plan(plan)) ROS_FATAL_STREAM("unable to plan");
+    move_group.move();
 
     sleep(3.0);
+  }
+
+private:
+  std::string getPartitionString(const char c, const std::size_t size) const {
+    std::string tmp(size, c);
+    return tmp;
   }
 };
 
@@ -62,10 +68,7 @@ int main(int argc, char** argv) {
 
   MoveGroupPlanner planner {"arcsys2"};
 
-  while (ros::ok()) {
-    ros::spin();
-    sleep(5.0);
-  }
+  ros::waitForShutdown();
 
   return 0;
 }
