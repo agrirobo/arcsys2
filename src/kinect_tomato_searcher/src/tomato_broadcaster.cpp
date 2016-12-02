e178e35abed2ba7b35117007e2c36432d87e7aab
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-12-03 02:01:27 +0900

    Update variable name for coding style

--- src/kinect_tomato_searcher/src/tomato_broadcaster.cpp
+++ src/kinect_tomato_searcher/src/tomato_broadcaster.cpp
@@ -3,34 +3,34 @@
 #include <geometry_msgs/TransformStamped.h>
 #include <tf2_ros/transform_broadcaster.h>
 
-int offsetX, offsetY, offsetZ;
+int offset_x, offset_y, offset_z;
 
 void pointCallback(const geometry_msgs::Point::ConstPtr& msg) {
   static tf2_ros::TransformBroadcaster br;
-  geometry_msgs::TransformStamped transformStamped;
+  geometry_msgs::TransformStamped transform_stamped;
   
-  transformStamped.header.stamp = ros::Time::now();
-  transformStamped.header.frame_id = "kinect";
-  transformStamped.child_frame_id = "tomato";
-  transformStamped.transform.translation.x = msg->x + offsetX;
-  transformStamped.transform.translation.y = msg->y + offsetY;
-  transformStamped.transform.translation.z = msg->z + offsetZ;
-  transformStamped.transform.rotation.x = 0;
-  transformStamped.transform.rotation.y = 0;
-  transformStamped.transform.rotation.z = 0;
-  transformStamped.transform.rotation.w = 1;
+  transform_stamped.header.stamp = ros::Time::now();
+  transform_stamped.header.frame_id = "kinect";
+  transform_stamped.child_frame_id = "tomato";
+  transform_stamped.transform.translation.x = msg->x + offset_x;
+  transform_stamped.transform.translation.y = msg->y + offset_y;
+  transform_stamped.transform.translation.z = msg->z + offset_z;
+  transform_stamped.transform.rotation.x = 0;
+  transform_stamped.transform.rotation.y = 0;
+  transform_stamped.transform.rotation.z = 0;
+  transform_stamped.transform.rotation.w = 1;
 
-  br.sendTransform(transformStamped);
+  br.sendTransform(transform_stamped);
 }
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "tomato_broadcaster");
   ros::NodeHandle node;
-  ros::NodeHandle private_node {"~"};
+  ros::NodeHandle offset_nh {"~offset"};
 
-  private_node.getParam("offset_x", offsetX);
-  private_node.getParam("offset_y", offsetY);
-  private_node.getParam("offset_z", offsetZ);
+  offset_nh.getParam("x", offset_x);
+  offset_nh.getParam("y", offset_y);
+  offset_nh.getParam("z", offset_z);
 
   ros::Subscriber sub = node.subscribe("target_point", 1, &pointCallback);
 
