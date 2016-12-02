61eca4277a291078fd3d29760abf6ef8f3681ac5
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-12-03 01:51:28 +0900

    Update topic name for generality

--- src/kinect_tomato_searcher/src/tomato_broadcaster.cpp
+++ src/kinect_tomato_searcher/src/tomato_broadcaster.cpp
@@ -26,13 +26,13 @@ void pointCallback(const geometry_msgs::Point::ConstPtr& msg) {
 int main(int argc, char** argv){
   ros::init(argc, argv, "tomato_broadcaster");
   ros::NodeHandle node;
-  ros::NodeHandle private_node;
+  ros::NodeHandle private_node {"~"};
 
-  private_node.getParam("kinect_offset_x", offsetX);
-  private_node.getParam("kinect_offset_y", offsetY);
-  private_node.getParam("kinect_offset_z", offsetZ);
+  private_node.getParam("offset_x", offsetX);
+  private_node.getParam("offset_y", offsetY);
+  private_node.getParam("offset_z", offsetZ);
 
-  ros::Subscriber sub = node.subscribe("/tomato_point", 1, &pointCallback);
+  ros::Subscriber sub = node.subscribe("target_point", 1, &pointCallback);
 
   ros::spin();
   return 0;
