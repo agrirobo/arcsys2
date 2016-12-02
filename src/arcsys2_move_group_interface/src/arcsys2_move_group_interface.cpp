e0085f343041e14dcfacd753a1ed057eba919064
YamaCIR-KIT <httperror@404-notfound.jp>
2016-12-02 16:41:45 +0900

    Change msg type to PointStamped from Point

--- src/arcsys2_move_group_interface/src/arcsys2_move_group_interface.cpp
+++ src/arcsys2_move_group_interface/src/arcsys2_move_group_interface.cpp
@@ -1,8 +1,8 @@
 #include <ros/ros.h>
 #include <moveit/move_group_interface/move_group.h>
 
-#include <geometry_msgs/Point.h>
-#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/PoseStamped.h>
 
 class MoveGroupInterface {
   moveit::planning_interface::MoveGroup move_group_;
@@ -12,18 +12,19 @@ class MoveGroupInterface {
 public:
   MoveGroupInterface(const std::string& group_name, ros::NodeHandle& node_handle)
     : move_group_ {group_name},
-      sub_ {node_handle.subscribe<geometry_msgs::Point>("/tomato_point", 1, &MoveGroupInterface::callback, this)}
+      sub_ {node_handle.subscribe<geometry_msgs::PointStamped>("/tomato_point_trusted", 1, &MoveGroupInterface::callback, this)}
   {
   }
 
 private:
-  void callback(const geometry_msgs::PointConstPtr& point)
+  void callback(const geometry_msgs::PointStampedConstPtr& msg)
   {
-    geometry_msgs::Pose pose;
-    pose.position = *point;
-    pose.orientation.w = 1.0;
+    geometry_msgs::PoseStamped target_pose;
+    target_pose.header = msg->header;
+    target_pose.pose.position = msg->point;
+    target_pose.pose.orientation.w = 1.0;
 
-    move_group_.setPoseTarget(pose);
+    move_group_.setPoseTarget(target_pose);
 
     moveit::planning_interface::MoveGroup::Plan plan;
     if (move_group_.plan(plan)) ROS_INFO_STREAM("SUCCESS");
@@ -37,8 +38,6 @@ int main(int argc, char** argv) {
   ros::init(argc, argv, "arcsys2_move_group_interface_node");
 
   ros::NodeHandle node_handle {"~"};
-
-  ros::Rate rate {ros::Duration(3.0)};
   ros::AsyncSpinner spinner {1};
 
   MoveGroupInterface interface {"arcsys2", node_handle};
