2fa57f6778d4ab246a556201998b1bf63fa1fcbf
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-12-03 15:32:02 +0900

    Update redFilter for more getting

--- src/kinect_tomato_searcher/src/kinect_tomato_searcher_node.cpp
+++ src/kinect_tomato_searcher/src/kinect_tomato_searcher_node.cpp
@@ -166,7 +166,7 @@ private:
     for(y = 0; y < hsv.rows; y++) {
       for(x = 0; x < hsv.cols; x++) {
         a = hsv.step*y+(x*3);
-        if((hsv.data[a] <= 5 || hsv.data[a] >= 175) && hsv.data[a+1] >= 50 && hsv.data[a+2] >= 50 )
+        if((hsv.data[a] <= 10 || hsv.data[a] >= 160) && hsv.data[a+1] >= 30 && hsv.data[a+2] >= 30)
           binary.at<unsigned char>(y,x) = 255;
         else
           binary.at<unsigned char>(y,x) = 0;
