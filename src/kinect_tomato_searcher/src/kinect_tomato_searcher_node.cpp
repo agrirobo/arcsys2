0a4388d11e2bf0ddacbba01c772092ec92e64e85
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-12-04 14:22:03 +0900

    Fix bug for publish

--- src/kinect_tomato_searcher/src/kinect_tomato_searcher_node.cpp
+++ src/kinect_tomato_searcher/src/kinect_tomato_searcher_node.cpp
@@ -294,8 +294,8 @@ public:
         tomato_pose.position.y = tomato_pose.position.x * tan(angle_x_per_piccell_ * tomato_pose.position.y) / 1000.0; // convert to m
         tomato_pose.position.z = tomato_pose.position.x * tan(angle_y_per_piccell_ * tomato_pose.position.z) / 1000.0; // convert to m
         tomato_pose.position.x = tomato_pose.position.x / 1000.0; // convert to m
-        poses_pub.publish(pub_msg);
       }
+      poses_pub.publish(pub_msg);
     }
 
     cv::waitKey(100);
