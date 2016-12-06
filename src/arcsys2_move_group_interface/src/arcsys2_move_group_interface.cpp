dcba604e1b23953253c8fab7f6a5036f112e6904
YamaCIR-KIT <httperror@404-notfound.jp>
2016-12-07 02:07:30 +0900

    Update to enable shift base position
    トマト未発見時に横へベースをシフトさせる部分を実装（関数hoge）
    
    カーテシアンパスだとレールギリギリかつ近めの座標を指定した時，
    融通の利かない動きをしたため
    収穫シーケンスの最初，トマト手前にエフェクタを持っていく動作を
    カーテシアンパスから普通のsetPoseTargetからplan，executeの動きに変更
    
    詳しい症状としては
    
    +--------R------------------+ rail
           ,-'
           T
    
    この状態からトマトをレールギリギリにすると
    
    +--------R------------------+
           ,-'
                               T
    
    カーテシアンパスだとどうも
    腕を反転させるという動きが出来ないらしく，
    こうなって
    
    +--------------------------R+ rail
                             ,-'
                               T
    
    パスが生成できなくなる

--- src/arcsys2_move_group_interface/src/arcsys2_move_group_interface.cpp
+++ src/arcsys2_move_group_interface/src/arcsys2_move_group_interface.cpp
@@ -9,7 +9,7 @@
 
 #include <geometry_msgs/Pose.h>
 #include <geometry_msgs/Point.h>
-#include <geometry_msgs/Transform.h>
+#include <geometry_msgs/TransformStamped.h>
 #include <moveit_msgs/RobotTrajectory.h>
 
 class MoveGroupInterface {
@@ -24,13 +24,17 @@ class MoveGroupInterface {
   static constexpr double eef_length_ {0.3}; // TODO
   static constexpr double eef_step_ {0.10};
 
+  static constexpr double abs_rail_length_ {5.0};
+  double sign_;
+
 public:
   MoveGroupInterface(const std::string& group_name, const double& joint_tolerance)
     : move_group_ {group_name},
       buffer_ {},
       listener_ {buffer_},
       tomapo_ {},
-      waypoints_ {}
+      waypoints_ {},
+      sign_ {1.0}
   {
     move_group_.allowReplanning(true);
     move_group_.setGoalJointTolerance(joint_tolerance);
@@ -40,7 +44,7 @@ public:
   bool queryTargetExistence()
   {
     try {
-      geometry_msgs::TransformStamped transform_stamped_ {buffer_.lookupTransform("rail", "tomato", ros::Time(0), ros::Duration(5.0))};
+      geometry_msgs::TransformStamped transform_stamped_ {buffer_.lookupTransform("rail", "tomato", ros::Time(0), ros::Duration(1.0))};
       tomapo_.position.x = transform_stamped_.transform.translation.x;
       tomapo_.position.y = transform_stamped_.transform.translation.y;
       tomapo_.position.z = transform_stamped_.transform.translation.z;
@@ -61,7 +65,12 @@ public:
 
     geometry_msgs::Pose pose1 {tomapo_};
     pose1.position.x -= eef_length_;
-    waypoints_.push_back(pose1);
+    // waypoints_.push_back(pose1);
+    move_group_.setPoseTarget(pose1);
+
+    moveit::planning_interface::MoveGroup::Plan plan;
+    move_group_.plan(plan);
+    if (!move_group_.execute(plan)) return false;
 
     geometry_msgs::Pose pose2 {tomapo_};
     waypoints_.push_back(pose2);
@@ -91,32 +100,48 @@ public:
     return move_group_.execute(motion_plan_);
   }
 
-private:
-  moveit_msgs::RobotTrajectory getCartesianPaths()
+  bool hoge()
   {
-    waypoints_.push_back(linear(tomapo_, -eef_length));
-    waypoints_.push_back(tomapo_);
-    waypoints_.push_back(roll(tomapo_, 1.0));
-    waypoints_.push_back(linear(tomapo_, -eef_length));
+    double tmp;
 
-    moveit_msgs::RobotTrajectory robot_trajectory_;
-    move_group_.computeCartesianPath(waypoints_, eef_step, 0.0, robot_trajectory_);
+    try {
+      geometry_msgs::TransformStamped transform_stamped {buffer_.lookupTransform("rail", "shaft", ros::Time(0), ros::Duration(1.0))};
+      tmp = transform_stamped.transform.translation.y;
+      if (tmp < (abs_rail_length_ - 1.0)) sign_ = 1.0;
+      else if (tmp > -(abs_rail_length_ - 1.0)) sign_ = -1.0;
+    } catch (const tf2::TransformException& ex) {
+      ROS_INFO_STREAM(ex.what());
+      return false;
+    }
 
-    return robot_trajectory_;
-  }
+    waypoints_.clear();
 
-  geometry_msgs::Pose linear(const geometry_msgs::Pose& pose, const double& distance)
-  {
-    geometry_msgs::Pose pose_ {pose};
-    pose_.position.x += distance;
-    return pose_;
-  }
+    geometry_msgs::Pose pose1 {};
+    pose1.position.x = 1.0; // TODO
+    pose1.position.y = tmp;
+    pose1.position.z = 1.5;
+    pose1.orientation.w = 1.0;
+    waypoints_.push_back(pose1);
 
-  geometry_msgs::Pose roll(const geometry_msgs::Pose& pose, const double& radian)
-  {
-    geometry_msgs::Pose pose_ {pose};
-    pose_.orientation = tf::createQuaternionMsgFromRollPitchYaw(radian, 0, 0);
-    return pose_;
+    geometry_msgs::Pose pose2 {pose1};
+    pose2.position.x += (sign_ * 1.0);
+    waypoints_.push_back(pose2);
+
+    moveit_msgs::RobotTrajectory trajectory_msgs_;
+    move_group_.computeCartesianPath(waypoints_, eef_step_, 0.0, trajectory_msgs_);
+
+    robot_trajectory::RobotTrajectory robot_trajectory_ {move_group_.getCurrentState()->getRobotModel(), move_group_.getName()};
+    robot_trajectory_.setRobotTrajectoryMsg(*move_group_.getCurrentState(), trajectory_msgs_);
+
+    trajectory_processing::IterativeParabolicTimeParameterization iptp;
+    iptp.computeTimeStamps(robot_trajectory_);
+
+    robot_trajectory_.getRobotTrajectoryMsg(trajectory_msgs_);
+
+    moveit::planning_interface::MoveGroup::Plan motion_plan_;
+    motion_plan_.trajectory_ = trajectory_msgs_;
+
+    return move_group_.execute(motion_plan_);
   }
 };
 
@@ -125,13 +150,16 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "arcsys2_move_group_interface_node");
   ros::NodeHandle node_handle {"~"};
 
-  MoveGroupInterface interface {"arcsys2", node_handle.param("joint_tolerance", 0.1)};
-
   ros::AsyncSpinner spinner {1};
   spinner.start();
 
+  MoveGroupInterface interface {"arcsys2", node_handle.param("joint_tolerance", 0.1)};
+
   while (ros::ok()) {
-    if (interface.queryTargetExistence()) interface.startSequence();
+    // if (interface.queryTargetExistence()) interface.startSequence();
+
+    while (!interface.queryTargetExistence()) interface.hoge();
+    interface.startSequence();
   }
 
   spinner.stop();
