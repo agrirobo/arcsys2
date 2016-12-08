5cc988a9459e4193c6858239129d7787b7ba7746
Doi Yusuke <o111027y@mail.kyutech.jp>
2016-12-08 21:45:14 +0900

    Add Calibrator class for registed real position

--- src/kinect_tomato_searcher/src/kinect_tomato_selector_node.cpp
+++ src/kinect_tomato_searcher/src/kinect_tomato_selector_node.cpp
@@ -1,11 +1,21 @@
 #include <ros/ros.h>
+#include <geometry_msgs/Pose.h>
 #include <geometry_msgs/PoseArray.h>
 #include <geometry_msgs/PoseStamped.h>
 
 #include <algorithm>
+#include <memory>
 #include <vector>
 
-ros::Publisher pub;
+class Calibrator
+{
+public:
+  geometry_msgs::Pose calibrate(geometry_msgs::Pose) const;
+private:
+  const double offset_x_;
+  const double offset_y_;
+  const double offset_z_;
+};
 
 class NearSelector
 {
@@ -18,14 +28,26 @@ private:
 void callback(const geometry_msgs::PoseArray&);
 double calcDiff(const geometry_msgs::Pose&, const geometry_msgs::Pose&);
 
+ros::Publisher pub;
+std::unique_ptr<Calibrator> calibrator;
+
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "kinect_tomato_selector_node");
   ros::NodeHandle tomato_nh {"tomato"};
   auto sub = tomato_nh.subscribe("array", 1, &callback);
   pub = tomato_nh.advertise<geometry_msgs::PoseStamped>("raw", 1);
+  calibrator = std::unique_ptr<Calibrator>(new Calibrator {});
   ros::spin();
   return 0;
+}
+
+inline geometry_msgs::Pose Calibrator::calibrate(geometry_msgs::Pose pose) const
+{
+  pose.position.x += offset_x_;
+  pose.position.y += offset_y_;
+  pose.position.z += offset_z_;
+  return pose;
 }
 
 inline geometry_msgs::Pose NearSelector::select(const geometry_msgs::PoseArray& pose_array)
@@ -57,6 +79,6 @@ void callback(const geometry_msgs::PoseArray& pose_array)
   static NearSelector selector {};
   geometry_msgs::PoseStamped pub_msg;
   pub_msg.header = pose_array.header;
-  pub_msg.pose = selector.select(pose_array);
+  pub_msg.pose = calibrator->calibrate(selector.select(pose_array));
   pub.publish(pub_msg);
 }
