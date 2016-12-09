6bdd85d607db8ecde93258b1b097d8dfce4a02ee
YamaCIR-KIT <httperror@404-notfound.jp>
2016-12-10 01:47:39 +0900

    Move planning cartesian path section to private func

--- src/arcsys2_move_group_interface/src/arcsys2_move_group_interface.cpp
+++ src/arcsys2_move_group_interface/src/arcsys2_move_group_interface.cpp
@@ -89,21 +89,22 @@ public:
     pose4.position.x -= eef_length_;
     waypoints_.push_back(pose4);
 
-    moveit_msgs::RobotTrajectory trajectory_msgs_;
-    move_group_.computeCartesianPath(waypoints_, 0.10, 0.0, trajectory_msgs_);
-
-    robot_trajectory::RobotTrajectory robot_trajectory_ {move_group_.getCurrentState()->getRobotModel(), move_group_.getName()};
-    robot_trajectory_.setRobotTrajectoryMsg(*move_group_.getCurrentState(), trajectory_msgs_);
-
-    trajectory_processing::IterativeParabolicTimeParameterization iptp;
-    iptp.computeTimeStamps(robot_trajectory_);
-
-    robot_trajectory_.getRobotTrajectoryMsg(trajectory_msgs_);
-
-    moveit::planning_interface::MoveGroup::Plan motion_plan_;
-    motion_plan_.trajectory_ = trajectory_msgs_;
-
-    return move_group_.execute(motion_plan_);
+    // moveit_msgs::RobotTrajectory trajectory_msgs_;
+    // move_group_.computeCartesianPath(waypoints_, 0.10, 0.0, trajectory_msgs_);
+    //
+    // robot_trajectory::RobotTrajectory robot_trajectory_ {move_group_.getCurrentState()->getRobotModel(), move_group_.getName()};
+    // robot_trajectory_.setRobotTrajectoryMsg(*move_group_.getCurrentState(), trajectory_msgs_);
+    //
+    // trajectory_processing::IterativeParabolicTimeParameterization iptp;
+    // iptp.computeTimeStamps(robot_trajectory_);
+    //
+    // robot_trajectory_.getRobotTrajectoryMsg(trajectory_msgs_);
+    //
+    // moveit::planning_interface::MoveGroup::Plan motion_plan_;
+    // motion_plan_.trajectory_ = trajectory_msgs_;
+    //
+    // return move_group_.execute(motion_plan_);
+    return move_group_.execute(planCartesianPath(waypoints_));
   }
 
   bool shift()
@@ -131,6 +132,24 @@ public:
     move_group_.plan(plan);
 
     return move_group_.execute(plan);
+  }
+
+private:
+  moveit::planning_interface::MoveGroup::Plan planCartesianPath(std::vector<geometry_msgs::Pose>& waypoints)
+  {
+    moveit_msgs::RobotTrajectory msg;
+    move_group_.computeCartesianPath(waypoints, 0.10, 0.0, msg);
+
+    robot_trajectory::RobotTrajectory trajectory {move_group_.getRobotModel(), move_group_.getName()};
+    trajectory.setRobotTrajectoryMsg(*move_group_.getCurrentState(), msg);
+
+    trajectory_processing::IterativeParabolicTimeParameterization iptp;
+    iptp.computeTimeStamps(trajectory);
+
+    moveit::planning_interface::MoveGroup::Plan plan;
+    plan.trajectory_ = msg;
+
+    return plan;
   }
 };
 
