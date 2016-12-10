a3ee899f71b9ecad86b4840b947b4592729d9ae6
YamaCIR-KIT <httperror@404-notfound.jp>
2016-12-10 19:48:22 +0900

    Update to match robot description
    変数名は動作に影響ないと思うので放置

--- src/arcsys2_control/src/arcsys2_control_node.cpp
+++ src/arcsys2_control/src/arcsys2_control_node.cpp
@@ -156,21 +156,20 @@ int main(int argc, char *argv[])
 
   hardware_interface::JointStateInterface joint_state_interface {};
   hardware_interface::PositionJointInterface position_joint_interface {};
-  // hardware_interface::VelocityJointInterface velocity_joint_interface {};
 
-  SimplePositionControl::BuildDataType shaft_builder {"rail_to_shaft_joint", joint_state_interface, position_joint_interface};
+  SimplePositionControl::BuildDataType shaft_builder {"rail_to_base_joint", joint_state_interface, position_joint_interface};
   SimplePositionControl shaft_control {shaft_builder};
   SimplePositionControl::BuildDataType arm0_builder {"shaft_to_arm0_joint", joint_state_interface, position_joint_interface};
   SimplePositionControl arm0_control {arm0_builder};
   auto ics_id_it = ics_ids.cbegin();
-  DammyPositionControl::BuildDataType arm1_builder {"arm0_to_arm1_joint", joint_state_interface, position_joint_interface};
-  DammyPositionControl arm1_control {arm1_builder};
-  DammyPositionControl::BuildDataType arm2_builder {"arm1_to_arm2_joint", joint_state_interface, position_joint_interface};
-  DammyPositionControl arm2_control {arm2_builder};
-  DammyPositionControl::BuildDataType effector_base_builder {"arm2_to_effector_base_joint", joint_state_interface, position_joint_interface};
-  DammyPositionControl effector_base_control {effector_base_builder};
-  DammyPositionControl::BuildDataType effector_end_builder {"effector_base_to_effector_end_joint", joint_state_interface, position_joint_interface};
-  DammyPositionControl effector_end_control {effector_end_builder};
+  ICSControl::BuildDataType arm1_builder {"arm0_to_arm1_joint", joint_state_interface, position_joint_interface};
+  ICSControl arm1_control {arm1_builder, ics_driver, *ics_id_it++};
+  ICSControl::BuildDataType arm2_builder {"arm1_to_arm2_joint", joint_state_interface, position_joint_interface};
+  ICSControl arm2_control {arm2_builder, ics_driver, *ics_id_it++};
+  ICSControl::BuildDataType effector_base_builder {"arm2_to_effector_joint", joint_state_interface, position_joint_interface};
+  ICSControl effector_base_control {effector_base_builder, ics_driver, *ics_id_it++};
+  ICSControl::BuildDataType effector_end_builder {"effector_to_blade_joint", joint_state_interface, position_joint_interface};
+  ICSControl effector_end_control {effector_end_builder, ics_driver, *ics_id_it++};
 
   Arcsys2HW robot {&joint_state_interface};
   robot.registerInterface(&position_joint_interface);
